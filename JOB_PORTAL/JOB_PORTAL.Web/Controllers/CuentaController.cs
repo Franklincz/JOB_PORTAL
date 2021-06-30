a7e3da6b0d68850ee50b9f68ad06ac74400fea48
Franklin Cunya <cunyazetafranklin@gmail.com>
2021-06-29 22:19:40 -0500

    final project-basic structure

---
+++ JOB_PORTAL/JOB_PORTAL.Web/Controllers/CuentaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOB_PORTAL.Web.Models
+{
+    public class CuentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LoginCandidato()
+        {
+
+            return View();
+
+        }
+        public IActionResult LoginAdEmpresa()
+        {
+
+
+
+            return View();
+        }
+
+
+    }
+}
